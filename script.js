66299f3cae3a10888e67223a4f3084d4378ebf59
JONATHAN HICKS <jonny@teachon.org>
2023-11-15 20:12:04 +0700

    feat: switchPlayer()

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ function createGame() {
     players: [],
     addPlayer: function (p) {
       p.makeMove = function (row, col) {
-        this.board[row][col] = p.name;
+        if (this.board[row][col] === null) {
+          this.board[row][col] = p.name;
+        }
       }.bind(gameInstance);
       this.players.push(p);
     },
@@ -27,11 +29,25 @@ const game = createGame();
 const jonny = createPlayer("jonny");
 const kevin = createPlayer("kevin");
 
+console.log(game.board);
+
+console.log(game.players);
+
 game.addPlayer(jonny);
 game.addPlayer(kevin);
 kevin.makeMove(1, 1);
-
-jonny.makeMove(1, 0);
-jonny.makeMove(1, 1);
+jonny.makeMove(0, 1);
+kevin.makeMove(0, 2);
+jonny.makeMove(0, 0);
 
 console.log(game);
+
+let activePlayer = game.players[0];
+console.log(activePlayer);
+
+const switchPlayer = () => {
+  activePlayer =
+    activePlayer === game.players[0] ? game.players[1] : game.players[0];
+};
+
+console.log(activePlayer);
